02e093d4f23b87fd364476a9b4099e2d0b19080a
khadija-bit <sumeyahassan34@gmail.com>
2020-04-06 17:29:12 +0300

    initial commit

---
+++ src/app/date-count.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { DateCountPipe } from './date-count.pipe';
+
+describe('DateCountPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateCountPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
